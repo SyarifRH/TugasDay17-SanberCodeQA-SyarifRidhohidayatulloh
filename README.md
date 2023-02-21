9682442283fcf5b665bfb8c2b966188be32cc589
ridho1610 <98494067+ridho1610@users.noreply.github.com>
2023-02-22 03:56:13 +0700

    Terbaru

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ Scenario Test Positive REGISTER
 - Test Case untuk Mendaftar dengan Username yang Sudah Terdaftar = PASSED
 
 Scenario Test Negative REGISTER
+
+- Test Case Nama yang kosong: Tidak mengisi kolom nama pada form register = PASSED
+- Test Case Password yang kosong: Tidak mengisi kolom password pada form register = PASSED
+- Test Case Mendaftar tanpa mengisi seluruh field yang wajib diisi: Mencoba untuk mendaftar tanpa mengisi field yang wajib diisi seperti nama, email dan password = PASSED
--- register.py
+++ register.py
@@ -10,11 +10,40 @@ class TestRegister(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome(ChromeDriverManager().install())
 
-        self.username = "ritter1",
-        self.email = "ritter11@gmail.com"
-        self.password = "12345"
+        # Rubahlah field isi data variable masing-masing dibawah ini, jika ingin test run lagi!
+        self.username = "SyarifRidho"
+        self.email = "syarifridho@gmail.com"
+        self.password = "123425"
 
-    # TEST CASE POSITIVE REGRISTER
+    # TEST CASE POSITIVE REGISTER
+
+    def test_registered_email(self):
+        # Test data register email terdaftar
+        driver = self.browser
+        driver.get("http://barru.pythonanywhere.com/daftar")
+        driver.find_element(By.ID, "signUp").click()
+        time.sleep(1)
+
+        name = driver.find_element(By.ID, "name_register")
+        name.send_keys(self.username)
+        email = driver.find_element(By.ID, "email_register")
+        email.send_keys(self.email)
+        password = driver.find_element(By.ID, "password_register")
+        password.send_keys(self.password)
+
+        btnRegister = driver.find_element(By.ID, "signup_register")
+        btnRegister.click()
+        time.sleep(3)
+
+        # validasi
+        response_data = driver.find_element(
+            By.CSS_SELECTOR, "#swal2-title").text
+        respons_message = driver.find_element(
+            By.CSS_SELECTOR, "#swal2-content").text
+
+        self.assertIn(response_data, 'Oops...')
+        self.assertEqual(respons_message, 'Gagal Register!')
+
     def test_register_success(self):
         # Test register data valid
         driver = self.browser
@@ -42,8 +71,37 @@ class TestRegister(unittest.TestCase):
         self.assertIn(response_data, 'berhasil')
         self.assertEqual(respons_message, 'created user!')
 
-    def test_register_email(self):
-        # Test data register email terdaftar
+    # TEST CASE NEGATIVE REGISTER
+
+    def test_blank_name(self):
+        # Test data nama kosong
+        driver = self.browser
+        driver.get("http://barru.pythonanywhere.com/daftar")
+        driver.find_element(By.ID, "signUp").click()
+        time.sleep(1)
+
+        name = driver.find_element(By.ID, "name_register")
+        name.send_keys("")
+        email = driver.find_element(By.ID, "email_register")
+        email.send_keys(self.email)
+        password = driver.find_element(By.ID, "password_register")
+        password.send_keys(self.password)
+
+        btnRegister = driver.find_element(By.ID, "signup_register")
+        btnRegister.click()
+        time.sleep(3)
+
+        # validasi
+        response_data = driver.find_element(
+            By.CSS_SELECTOR, "#swal2-title").text
+        respons_message = driver.find_element(
+            By.CSS_SELECTOR, "#swal2-content").text
+
+        self.assertIn(response_data, 'Oops...')
+        self.assertEqual(respons_message, 'Gagal Register!')
+
+    def test_blank_password(self):
+        # Test data password kosong
         driver = self.browser
         driver.get("http://barru.pythonanywhere.com/daftar")
         driver.find_element(By.ID, "signUp").click()
@@ -54,7 +112,34 @@ class TestRegister(unittest.TestCase):
         email = driver.find_element(By.ID, "email_register")
         email.send_keys(self.email)
         password = driver.find_element(By.ID, "password_register")
-        password.send_keys(self.password)
+        password.send_keys("")
+
+        btnRegister = driver.find_element(By.ID, "signup_register")
+        btnRegister.click()
+        time.sleep(3)
+
+        # validasi
+        response_data = driver.find_element(
+            By.CSS_SELECTOR, "#swal2-title").text
+        respons_message = driver.find_element(
+            By.CSS_SELECTOR, "#swal2-content").text
+
+        self.assertIn(response_data, 'Oops...')
+        self.assertEqual(respons_message, 'Gagal Register!')
+
+    def test_all_blank(self):
+        # Test data all blank
+        driver = self.browser
+        driver.get("http://barru.pythonanywhere.com/daftar")
+        driver.find_element(By.ID, "signUp").click()
+        time.sleep(1)
+
+        name = driver.find_element(By.ID, "name_register")
+        name.send_keys("")
+        email = driver.find_element(By.ID, "email_register")
+        email.send_keys("")
+        password = driver.find_element(By.ID, "password_register")
+        password.send_keys("")
 
         btnRegister = driver.find_element(By.ID, "signup_register")
         btnRegister.click()
